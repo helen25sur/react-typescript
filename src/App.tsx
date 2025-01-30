aca2b96b2c1c0b5505be8823ada325ebffe68865
Olena Surilova <elen21surilova@gmail.com>
2025-01-30 12:40:54 +0100

    essentials-ts-react: add the second component Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import Header from "./components/Header";
 import CourseGoal from "./components/CourseGoal";
 
 import "./App.css";
 
+import goalsImg from './assets/goals.jpg';
+
 export default function App() {
   return (
     <main>
+      <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
+        <h1>Your Course Goals</h1>
+      </Header>
       <ul>
         <li>
           <CourseGoal title="Learn React + TS">
