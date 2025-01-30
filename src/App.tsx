b25ec224f291bc0bbf02f377e3509e6a50992886
Olena Surilova <elen21surilova@gmail.com>
2025-01-30 13:55:22 +0100

    essentials-ts-react: add button Add Goal and handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import CourseGoal from "./components/CourseGoal";
 
@@ -5,18 +7,40 @@ import "./App.css";
 
 import goalsImg from './assets/goals.jpg';
 
+interface CourseGoalObj {
+  title: string;
+  description: string;
+  id: number;
+}
+
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoalObj[]>([]);
+
+  function handleAddGoal() {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoalObj = {
+        title: 'Learn React + TS',
+        description: 'Learn it in depth!',
+        id: Math.random(),
+      }
+      return [...prevGoals, newGoal]
+    })
+  }
+
   return (
     <main>
       <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
         <h1>Your Course Goals</h1>
       </Header>
+      <button onClick={handleAddGoal}>Add Goal</button>
       <ul>
-        <li>
-          <CourseGoal title="Learn React + TS">
-            <p>Learn it from the ground up</p>
-          </CourseGoal>
-        </li>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
       </ul>
     </main>
   );
